b18a2715a6f958c5b8e60386067d9f014dafe5ab
gekuo888-lgtm <gekuo888@gmail.com>
2026-06-03 21:31:38 +0800

    Update stock.py

--- stock.py
+++ stock.py
@@ -1,29 +1,15 @@
 import os
 import requests
+import yfinance as yf
 
 TOKEN = os.getenv("LINE_TOKEN")
 USER_ID = os.getenv("LINE_USER_ID")
 
-headers = {
-    "Authorization": f"Bearer {TOKEN}",
-    "Content-Type": "application/json"
+stocks = {
+    "0050.TW": "0050 元大台灣50",
+    "0052.TW": "0052 富邦科技",
+    "0056.TW": "0056 元大高股息",
+    "00919.TW": "00919 群益台灣精選高息",
+    "2330.TW": "2330 台積電",
+    "3665.TW": "3665 貿聯-KY"
 }
-
-payload = {
-    "to": USER_ID,
-    "messages": [
-        {
-            "type": "text",
-            "text": "GitHub 成功連接 LINE 🎉"
-        }
-    ]
-}
-
-response = requests.post(
-    "https://api.line.me/v2/bot/message/push",
-    headers=headers,
-    json=payload
-)
-
-print(response.status_code)
-print(response.text)
